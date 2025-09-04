c1b9f3e32a87e73a8cdc8395a7044ab6fe7f6b94
Maylin Esthefany Jauregui Gamboa <112034315+MAYLIN-ESTHEFANY-JAUREGUI-GAMBOA@users.noreply.github.com>
2025-09-04 11:24:48 +0000

    Consumiendo ApisAI De Rapidapi

--- src/main/java/pe/edu/vallegrande/ApiAI/service/impl/ImageProcessingServiceImpl.java
+++ src/main/java/pe/edu/vallegrande/ApiAI/service/impl/ImageProcessingServiceImpl.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
